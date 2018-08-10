3a66d074be89f3d149c7cb7f4c453d338a278a3c
Vova Danilchenko <vovadanilchenkoo@gmail.com>
2018-08-10 10:37:30 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  .container
+    .row
+      .col.s8.offset-s2
+        AppDataTable
+</template>
+
+
+<script>
+import AppDataTable from './components/AppDataTable.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppDataTable
+  }
+}
+</script>
+
+<style>
+
+</style>
