3733e3bacea9405ecad0e027faccbbc229f659bc
Vova Danilchenko <vovadanilchenkoo@gmail.com>
2018-08-17 00:08:29 +0300

    temp

--- src/components/AppDataTable.vue
+++ src/components/AppDataTable.vue
@@ -12,9 +12,9 @@
       thead
         tr
           th ID
-          th(ref='th' @click='sortTable($event)') Name
-          th(ref='th' @click='sortTable($event)') Location
-          th(ref='th' @click='sortTable($event)') Currency
+          th Name
+          th Location
+          th Currency
       tbody
         tr(v-for='testItem in filteredList')
           td(ref='td' class='data-table__td' @click='editCell') {{ testItem.id }}
@@ -60,44 +60,44 @@ export default {
 
     var sortOrder;
     var col;
-  $('th').each(function(col) {
-    $(this).click(function() {
-      if ($(this).is('.asc')) {
-        $(this).removeClass('asc');
-        $(this).addClass('desc selected');
-        sortOrder = -1;
-      } else {
-        $(this).addClass('asc selected');
-        $(this).removeClass('desc');
-        sortOrder = 1;
-      };
-      
-      $(this).siblings().removeClass('asc selected');
-      $(this).siblings().removeClass('desc selected');
-      // arrData - arr of all tr
-      var arrData = $('table').find('tbody >tr:has(td)').get();
-      
-      arrData.sort(function(a, b) {
-        // a, b - tr
-        
-        // eq(col) - find td in current clicked column
-        var val1 = $(a).children('td').eq(col).text().toUpperCase(); 
-        var val2 = $(b).children('td').eq(col).text().toUpperCase();
-        console.log(val1 < val2);
-        
-        if ($.isNumeric(val1) && $.isNumeric(val2)) {
-          return sortOrder == 1 ? val1 - val2 : val2 - val1;
+    $('th').each(function(col) {
+      $(this).click(function() {
+        console.log(col);
+        if ($(this).is('.asc')) {
+          $(this).removeClass('asc');
+          $(this).addClass('desc selected');
+          sortOrder = -1;
         } else {
-          return (val1 < val2) ? -sortOrder : (val1 > val2) ? sortOrder : 0;
-        }
+          $(this).addClass('asc selected');
+          $(this).removeClass('desc');
+          sortOrder = 1;
+        };
         
-      });
-      
-      $.each(arrData, function(index, row) {
-        $('tbody').append(row);
-      });
-    });    
-  });
+        $(this).siblings().removeClass('asc selected');
+        $(this).siblings().removeClass('desc selected');
+        // arrData - arr of all tr
+        var arrData = $('table').find('tbody >tr:has(td)').get();
+        
+        arrData.sort(function(a, b) {
+          // a, b - tr
+          
+          // eq(col) - find td in current clicked column
+          var val1 = $(a).children('td').eq(col).text().toUpperCase(); 
+          var val2 = $(b).children('td').eq(col).text().toUpperCase();
+          
+          if ($.isNumeric(val1) && $.isNumeric(val2)) {
+            return sortOrder == 1 ? val1 - val2 : val2 - val1;
+          } else {
+            return (val1 < val2) ? -sortOrder : (val1 > val2) ? sortOrder : 0;
+          }
+          
+        });
+        
+        $.each(arrData, function(index, row) {
+          $('tbody').append(row);
+        });
+      });    
+    });
 
   }
 }
